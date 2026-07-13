94a0433437e54dcdb1786dd4f19a2c2c3e83cece
Kriwit.j <j.kriwit@gmail.com>
2026-07-13 07:12:26 +0700

    first deploy

--- prisma.config.js
+++ prisma.config.js
@@ -16,6 +16,7 @@ module.exports = {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "node prisma/seed.js",
   },
   datasource: {
     url: process.env.DATABASE_URL,
